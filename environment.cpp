08fa296d33321539a1f1f0817a091586ada639f8
chris-blackburn <chrisnblackburn@gmail.com>
2018-09-06 15:56:36 -0400

    Euler's Number and Example for Average
    Implemented euler's number as a built-in symbol, and added an example of finding an average in plotscript

--- environment.cpp
+++ environment.cpp
@@ -6,7 +6,7 @@
 #include "environment.hpp"
 #include "semantic_error.hpp"
 
-/*********************************************************************** 
+/***********************************************************************
 Helper Functions
 **********************************************************************/
 
@@ -15,7 +15,7 @@ bool nargs_equal(const std::vector<Expression> & args, unsigned nargs){
   return args.size() == nargs;
 }
 
-/*********************************************************************** 
+/***********************************************************************
 Each of the functions below have the signature that corresponds to the
 typedef'd Procedure function pointer.
 **********************************************************************/
@@ -32,7 +32,7 @@ Expression add(const std::vector<Expression> & args){
   double result = 0;
   for( auto & a :args){
     if(a.isHeadNumber()){
-      result += a.head().asNumber();      
+      result += a.head().asNumber();
     }
     else{
       throw SemanticError("Error in call to add, argument not a number");
@@ -43,12 +43,12 @@ Expression add(const std::vector<Expression> & args){
 };
 
 Expression mul(const std::vector<Expression> & args){
- 
+
   // check all aruments are numbers, while multiplying
   double result = 1;
   for( auto & a :args){
     if(a.isHeadNumber()){
-      result *= a.head().asNumber();      
+      result *= a.head().asNumber();
     }
     else{
       throw SemanticError("Error in call to mul, argument not a number");
@@ -75,7 +75,7 @@ Expression subneg(const std::vector<Expression> & args){
     if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
       result = args[0].head().asNumber() - args[1].head().asNumber();
     }
-    else{      
+    else{
       throw SemanticError("Error in call to subtraction: invalid argument.");
     }
   }
@@ -88,13 +88,13 @@ Expression subneg(const std::vector<Expression> & args){
 
 Expression div(const std::vector<Expression> & args){
 
-  double result = 0;  
+  double result = 0;
 
   if(nargs_equal(args,2)){
     if( (args[0].isHeadNumber()) && (args[1].isHeadNumber()) ){
       result = args[0].head().asNumber() / args[1].head().asNumber();
     }
-    else{      
+    else{
       throw SemanticError("Error in call to division: invalid argument.");
     }
   }
@@ -114,13 +114,13 @@ Environment::Environment(){
 
 bool Environment::is_known(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   return envmap.find(sym.asSymbol()) != envmap.end();
 }
 
 bool Environment::is_exp(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ExpressionType);
 }
@@ -128,7 +128,7 @@ bool Environment::is_exp(const Atom & sym) const{
 Expression Environment::get_exp(const Atom & sym) const{
 
   Expression exp;
-  
+
   if(sym.isSymbol()){
     auto result = envmap.find(sym.asSymbol());
     if((result != envmap.end()) && (result->second.type == ExpressionType)){
@@ -144,18 +144,18 @@ void Environment::add_exp(const Atom & sym, const Expression & exp){
   if(!sym.isSymbol()){
     throw SemanticError("Attempt to add non-symbol to environment");
   }
-    
+
   // error if overwriting symbol map
   if(envmap.find(sym.asSymbol()) != envmap.end()){
     throw SemanticError("Attempt to overwrite symbol in environemnt");
   }
 
-  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp)); 
+  envmap.emplace(sym.asSymbol(), EnvResult(ExpressionType, exp));
 }
 
 bool Environment::is_proc(const Atom & sym) const{
   if(!sym.isSymbol()) return false;
-  
+
   auto result = envmap.find(sym.asSymbol());
   return (result != envmap.end()) && (result->second.type == ProcedureType);
 }
@@ -181,19 +181,22 @@ then re-add the default ones.
 void Environment::reset(){
 
   envmap.clear();
-  
+
   // Built-In value of pi
   envmap.emplace("pi", EnvResult(ExpressionType, Expression(PI)));
 
+	// Built_In value of euler's number
+	envmap.emplace("e", EnvResult(ExpressionType, Expression(EXP)));
+
   // Procedure: add;
-  envmap.emplace("+", EnvResult(ProcedureType, add)); 
+  envmap.emplace("+", EnvResult(ProcedureType, add));
 
   // Procedure: subneg;
-  envmap.emplace("-", EnvResult(ProcedureType, subneg)); 
+  envmap.emplace("-", EnvResult(ProcedureType, subneg));
 
   // Procedure: mul;
-  envmap.emplace("*", EnvResult(ProcedureType, mul)); 
+  envmap.emplace("*", EnvResult(ProcedureType, mul));
 
   // Procedure: div;
-  envmap.emplace("/", EnvResult(ProcedureType, div)); 
+  envmap.emplace("/", EnvResult(ProcedureType, div));
 }
---
+++ tests/example3.pls
@@ -0,0 +1,6 @@
+; Find the average of the numbers: 42, 34, 89, -3, 95, 4, -9, 32
+(begin
+	(define sum (+ 42 34 89 -3 95 4 -9 32))
+	(define n 8)
+	(/ sum n)
+)
