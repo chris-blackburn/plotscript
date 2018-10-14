54b5ab9532a62b68342f3ea6cb1bd7dddc08e2e8
chris-blackburn <chrisnblackburn@gmail.com>
2018-10-14 12:12:08 -0400

    Added point object to startup
    plotscript now creates a point object creating lambda function at startup called make-point. Also wrote some integration tests for it

--- scripts/integration_test.py
+++ scripts/integration_test.py
@@ -2,7 +2,7 @@ import pexpect
 import pexpect.replwrap as replwrap
 import unittest
 import os
-        
+
 # the plotscript executable
 cmd = './plotscript'
 
@@ -11,68 +11,82 @@ prompt = u'plotscript>'
 
 class TestREPL(unittest.TestCase):
 
-        def setUp(self):
-                self.wrapper = replwrap.REPLWrapper(cmd, prompt, None)
-                
-        def test_add(self):
-                output = self.wrapper.run_command(u'(+ 1 2)')
-                self.assertEqual(output.strip(), "(3)")
-                output = self.wrapper.run_command(u'(+ 1 2 10)')
-                self.assertEqual(output.strip(), "(13)")
-                output = self.wrapper.run_command(u'(+ 1 2 0)')
-                self.assertEqual(output.strip(), "(3)")
-                output = self.wrapper.run_command(u'(+ 0 1 2)')
-                self.assertEqual(output.strip(), "(3)")
-                output = self.wrapper.run_command(u'(+ 1 2 -2)')
-                self.assertEqual(output.strip(), "(1)")
-                output = self.wrapper.run_command(u'(+ -1 -2)')
-                self.assertEqual(output.strip(), "(-3)")
+	def setUp(self):
+		self.wrapper = replwrap.REPLWrapper(cmd, prompt, None)
 
-        def test_define(self):
-                output = self.wrapper.run_command(u'(define a 1)')
-                self.assertEqual(output.strip(), "(1)")
-                output = self.wrapper.run_command(u'(define b a)')
-                self.assertEqual(output.strip(), "(1)")
-                output = self.wrapper.run_command(u'(+ a b)')
-                self.assertEqual(output.strip(), "(2)")
+	def test_add(self):
+		output = self.wrapper.run_command(u'(+ 1 2)')
+		self.assertEqual(output.strip(), "(3)")
+		output = self.wrapper.run_command(u'(+ 1 2 10)')
+		self.assertEqual(output.strip(), "(13)")
+		output = self.wrapper.run_command(u'(+ 1 2 0)')
+		self.assertEqual(output.strip(), "(3)")
+		output = self.wrapper.run_command(u'(+ 0 1 2)')
+		self.assertEqual(output.strip(), "(3)")
+		output = self.wrapper.run_command(u'(+ 1 2 -2)')
+		self.assertEqual(output.strip(), "(1)")
+		output = self.wrapper.run_command(u'(+ -1 -2)')
+		self.assertEqual(output.strip(), "(-3)")
 
-        def test_error(self):
-                output = self.wrapper.run_command(u'(define begin True)')
-                self.assertTrue(output.strip().startswith('Error'))
-                                
+	def test_define(self):
+		output = self.wrapper.run_command(u'(define a 1)')
+		self.assertEqual(output.strip(), "(1)")
+		output = self.wrapper.run_command(u'(define b a)')
+		self.assertEqual(output.strip(), "(1)")
+		output = self.wrapper.run_command(u'(+ a b)')
+		self.assertEqual(output.strip(), "(2)")
+
+	def test_error(self):
+		output = self.wrapper.run_command(u'(define begin True)')
+		self.assertTrue(output.strip().startswith('Error'))
+
+	def test_graphic_primitives(self):
+
+		# Default point
+		output = self.wrapper.run_command(u'(make-point 17 5)')
+		self.assertEqual(output.strip(), "((17) (5))")
+		output = self.wrapper.run_command(u'(get-property "object-name" (make-point 17 5))')
+		self.assertEqual(output.strip(), '("point")')
+		output = self.wrapper.run_command(u'(get-property "size" (make-point 17 5))')
+		self.assertEqual(output.strip(), "(0)")
+
+		# Default line
+
+		# Default text
+
 class TestExecuteCommandline(unittest.TestCase):
-                
-        def test_sub(self):
-                args = ' -e ' + ' "(- 4 2)" '
-                (output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
-                self.assertEqual(retcode, 0)
-                self.assertEqual(output.strip(), b"(2)")
 
-        def test_error(self):
-                args = ' -e ' + ' "(- 4 2 12)" '
-                (output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
-                self.assertNotEqual(retcode, 0)
-                self.assertTrue(output.strip().startswith(b'Error'))
+	def test_sub(self):
+		args = ' -e ' + ' "(- 4 2)" '
+		(output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
+		self.assertEqual(retcode, 0)
+		self.assertEqual(output.strip(), b"(2)")
+
+	def test_error(self):
+		args = ' -e ' + ' "(- 4 2 12)" '
+		(output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
+		self.assertNotEqual(retcode, 0)
+		self.assertTrue(output.strip().startswith(b'Error'))
 
 class TestExecuteFromFile(unittest.TestCase):
-                
-        def test_unix(self):
-                args = ' /vagrant/tests/unix/test.pls'
-                (output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
-                self.assertEqual(retcode, 0)
-                self.assertEqual(output.strip(), b"(-4)")
 
-        def test_win(self):
-                args = ' /vagrant/tests/win/test.pls'
-                (output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
-                self.assertEqual(retcode, 0)
-                self.assertEqual(output.strip(), b"(-4)")
+	def test_unix(self):
+		args = ' /vagrant/tests/unix/test.pls'
+		(output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
+		self.assertEqual(retcode, 0)
+		self.assertEqual(output.strip(), b"(-4)")
 
-        def test_error(self):
-                args = ' /there/is/no/such/file'
-                (output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
-                self.assertNotEqual(retcode, 0)
-                self.assertTrue(output.strip().startswith(b'Error'))
+	def test_win(self):
+		args = ' /vagrant/tests/win/test.pls'
+		(output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
+		self.assertEqual(retcode, 0)
+		self.assertEqual(output.strip(), b"(-4)")
+
+	def test_error(self):
+		args = ' /there/is/no/such/file'
+		(output, retcode) = pexpect.run(cmd+args, withexitstatus=True, extra_args=args)
+		self.assertNotEqual(retcode, 0)
+		self.assertTrue(output.strip().startswith(b'Error'))
 
 # run the tests
 unittest.main()
--- startup.pls
+++ startup.pls
@@ -1,3 +1,15 @@
 (begin
-	(define sum (lambda (x y) (+ x y)))
+
+	; Creation of a point object
+	; Returns a list with property "object-name": "point", "size": 0
+	(define make-point (lambda (x y)
+		(set-property "object-name" "point"
+		(set-property "size" 0
+			(list x y)
+		))
+	))
+
+	; Creation of a line object
+
+	; Creation of a text object
 )
