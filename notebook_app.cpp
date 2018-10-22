0bc67acd5f267a96e26a8cda9042cd3e210dc544
chris-blackburn <chrisnblackburn@gmail.com>
2018-10-22 10:21:38 -0400

    Created notebook app widget
    Created a new class for the GUI called notebook_app. This class will house the parent widget for the notebook GUI application.

---
+++ notebook_app.cpp
@@ -0,0 +1,9 @@
+#include "notebook_app.hpp"
+
+NotebookApp::NotebookApp(QWidget* parent) {
+
+	// Keep compiler from compaining
+	if (parent) {
+		return;
+	}
+}
---
+++ notebook_app.hpp
@@ -0,0 +1,12 @@
+#ifndef NOTEBOOK_APP_HPP
+#define NOTEBOOK_APP_HPP
+
+#include <QWidget>
+
+class NotebookApp: public QWidget {
+Q_OBJECT
+public:
+	NotebookApp(QWidget* parent = nullptr);
+};
+
+#endif
